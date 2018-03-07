d89cff930a3ea038332ca2658e9ef942ba726689
Nikolai.Stoilkov <nikolai.stoilko98@gmail.com>
2018-03-08 00:49:41 +0200

    solved Problem01

---
+++ Problem 01/p01/CreditCard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p02
+{
+    class CreditCard
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int num4 = 0;
+
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    num1 += number;
+                }
+                else if (i == 1)
+                {
+                    num2 += number;
+                }
+                else if (i == 2)
+                {
+                    num3 += number;
+                }
+                else if (i == 3)
+                {
+                    num4 += number;
+                }
+
+            }
+
+            Console.WriteLine($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
+        }
+    }
+}
